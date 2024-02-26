63860a6fc298d60361781a6c30d9ba780e89352f
Tor Andersson <tor@ccxvii.net>
2024-02-26 19:45:43 +0100

    Add symlinks.

---
+++ symlink.sh
@@ -0,0 +1,33 @@
+
+ln -sf question_arrow help
+ln -sf hand2 pointer
+ln -sf watch wait
+ln -sf watch progress
+ln -sf xterm text
+ln -sf fleur move
+ln -sf sb_h_double_arrow ew-resize
+ln -sf sb_v_double_arrow ns-resize
+ln -sf cross cell
+
+ln -sf right_ptr context-menu
+ln -sf crosshair vertical-text
+
+ln -sf X_cursor not-allowed
+ln -sf X_cursor no-drop
+
+ln -sff icon grab
+ln -sff icon grabbing
+
+ln -sf fleur all-scroll
+
+ln -sf sizing nesw-resize
+ln -sf sizing nwse-resize
+
+ln -sf sb_v_double_arrow row-resize
+ln -sf sb_h_double_arrow col-resize
+
+ln -sf exchange alias
+ln -sf plus copy
+
+# zoom-in
+# zoom-out
